4b4d97dbc4524f5fcec5aefc00d8cf5f310dbff8
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2013-03-05 20:25:57 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mediator::Application.config.secret_key_base = '47af2edfb343878d286fa354fe5b433fce5fd1a7e911317957a804b4adb6d76a1db0ff5728bc07891581f23106714d4de0e77db8daa6e01e4c1bbe550f900425'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mediator::Application.config.session_store :encrypted_cookie_store, key: '_Mediator_session'
