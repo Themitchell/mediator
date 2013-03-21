a3189d886f17f0d6a703523e4f536414026b46a9
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2013-03-21 19:32:39 +0000

    A litte dryer

--- spec/importers/picture_importer_spec.rb
+++ spec/importers/picture_importer_spec.rb
@@ -8,27 +8,25 @@ describe PictureImporter do
     EXIFR::JPEG.any_instance.stub(:gps_lat).and_return 5
     EXIFR::JPEG.any_instance.stub(:gps_lng).and_return 3
     EXIFR::JPEG.any_instance.stub(:date_time).and_return "Sat Jan 01 00:00:00 +0000 2000"
+    subject.start
+    @picture = Picture.first
   end
 
   it 'sets the name to be the filename' do
-    subject.start
-    Picture.first.name.should eql 'test.jpg'
+    @picture.name.should eql 'test.jpg'
   end
 
   it 'sets the file' do
-    subject.start
-    Picture.first.file.should_not be_nil
+    @picture.file.should_not be_nil
   end
 
   it 'sets gps coordinates' do
-    subject.start
-    Picture.first.lat.should  eql 5.0
-    Picture.first.lng.should eql 3.0
+    @picture.lat.should  eql 5.0
+    @picture.lng.should eql 3.0
   end
 
   it 'sets the file_creation_date' do
-    subject.start
-    Picture.first.file_creation_date.should eql Time.parse("Sat, 01 Jan 2000 00:00:00 +00:00")
+    @picture.file_creation_date.should eql Time.parse("Sat, 01 Jan 2000 00:00:00 +00:00")
   end
 
 end
